d3053e8d0dcca6f1b97a5c856f9f4eb5925280a7
Prasann0009 <prasannlasod0009@gmail.com>
2024-06-04 20:13:34 +0530

    upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+// import { BrowserRouter,Route,Routes } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" Component={App}/>
-    </Routes>
-    </BrowserRouter>
-);
+root.render(<App/>);
